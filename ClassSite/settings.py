06c5cd3aaba397bf346c22e3b68737ec5aa7fae2
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-09-25 19:55:14 -0400

    Final Update before Mobile Revamp.

--- ClassSite/settings.py
+++ ClassSite/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sa7t=6q543^*df=p!ndh)zr+2@t0(v$^to+yp#%3pj#kptk&59'
-# SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'sa7t=6q543^*df=p!ndh)zr+2@t0(v$^to+yp#%3pj#kptk&59'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ['DEBUG']
+# DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 # db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
 # DATABASES['default'].update(db_from_env)
