646a6e2f3c830ca1a15863ecbba638daed51fb64
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-09-25 20:06:56 -0400

    Update debug

--- ClassSite/settings.py
+++ ClassSite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DEBUG')
+DEBUG = bool(os.environ.get('DEBUG'))
 
 # db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
 # DATABASES['default'].update(db_from_env)
